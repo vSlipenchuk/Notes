2221943270a03573ac4ba731cf1617f4b89dbe2c
Vladimir Slipenchuk <vSlipenchuk@gmail.com>
2014-03-26 16:12:59 +0400

    fixed cal-button

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,10 +1,5 @@
-/*
-<script>
-  Тут и хранятся константы приложения. Их позже можно вынимать
-  Важный параметр - defaultChromeURI - это главная запускаемая форма!!!
-*/
 //pref("toolkit.defaultChromeURI", "chrome://myapp/content/main.xul");
 pref("toolkit.defaultChromeURI", "chrome://myapp/content/nb.xul");
 pref("toolkit.defaultChromeFeatures", "chrome,resizable,centerscreen,dialog=no");
 pref("notes.db", "my_db_path");
-
+pref("toolkit.singletonWindowType", "xulmine"); 
