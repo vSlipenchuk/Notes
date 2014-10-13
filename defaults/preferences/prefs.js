f42a97c11d18e33d3574e7d3567c34cbdcc978b7
Vladimir Slipenchuk <vSlipenchuk@gmail.com>
2014-10-13 20:13:40 +0400

    fixed tags find

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -3,3 +3,9 @@ pref("toolkit.defaultChromeURI", "chrome://myapp/content/nb.xul");
 pref("toolkit.defaultChromeFeatures", "chrome,resizable,centerscreen,dialog=no");
 pref("notes.db", "my_db_path");
 pref("toolkit.singletonWindowType", "xulmine"); 
+
+
+
+pref("nglayout.debug.disable_xul_cache", true);
+pref("nglayout.debug.disable_xul_fastload", true);
+
